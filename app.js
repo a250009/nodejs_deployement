29b3c30d77f8518231acafa0e5a9d89dbdcd6428
a250009 <a250009@famt.ac.in>
2026-04-24 21:46:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write("Hello from Node.js deployed via Jenkins!");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(`
+    <html>
+      <body style="background-color:white; text-align:center;">
+        <h1>Hello from Node.js deployed via Jenkins!</h1>
+      </body>
+    </html>
+  `);
   res.end();
 });
 
